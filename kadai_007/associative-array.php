1d812219fdcb1a7276102bfecf90fef3a3cf62ec
anna-s1994 <nya.plu@yahoo.ne.jp>
2025-04-12 23:21:53 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>kadai_007</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="kadai_007です">
+  </head>
+
+  <body>
+    <?php
+      $associative_array= ['name' =>'onion', 'price' => 200, 'weight' => 160];
+      echo '<br>';
+      print_r($associative_array);
+      ?>
+  </body>
+
+</html>
